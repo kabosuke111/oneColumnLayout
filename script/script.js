67550815cd1491611e6a860d227908e30c01a58a
koke <mamechips@hotmail.com>
2019-06-30 17:29:02 +0900

    写真やスクリプトなど新たなファイルを追加

---
+++ script/script.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+
+var textPlus = document.getElementById("main");
+var textElem = document.createElement("p");
+textElem.style.fontSize = "28px";
+textPlus.appendChild(textElem).textContent = "ほせ";
