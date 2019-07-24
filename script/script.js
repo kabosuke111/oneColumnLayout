b2d3a6e072a268a8f5eeaa74e84f47e8f9417306
koke <mamechips@hotmail.com>
2019-07-24 23:32:47 +0900

    レイアウト、デザインを作成。
    全体のレイアウトを製作して、デザインも行った。レスポンシブにも対応した。

--- script/script.js
+++ script/script.js
@@ -1,7 +1,2 @@
 // JavaScript Document
 
-
-var textPlus = document.getElementById("main");
-var textElem = document.createElement("p");
-textElem.style.fontSize = "28px";
-textPlus.appendChild(textElem).textContent = "ほせ";
